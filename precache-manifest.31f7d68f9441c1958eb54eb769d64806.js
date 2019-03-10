8a14ef1592fe38e44ee4873cea5b37707815287a
Nami <nami5504@gmail.com>
2019-03-10 12:00:14 -0700

    Updates

---
+++ precache-manifest.31f7d68f9441c1958eb54eb769d64806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f850d9fe8590343c94c",
+    "url": "/team-orange/static/js/runtime~main.c98bde69.js"
+  },
+  {
+    "revision": "51253101f1da58f535e7",
+    "url": "/team-orange/static/js/main.8eadfafc.chunk.js"
+  },
+  {
+    "revision": "5717b9bbb1adb391d771",
+    "url": "/team-orange/static/js/2.55cf7d84.chunk.js"
+  },
+  {
+    "revision": "51253101f1da58f535e7",
+    "url": "/team-orange/static/css/main.d3c98182.chunk.css"
+  },
+  {
+    "revision": "5717b9bbb1adb391d771",
+    "url": "/team-orange/static/css/2.a7b99923.chunk.css"
+  },
+  {
+    "revision": "019ad2698023a04903c178acbfcf7ccd",
+    "url": "/team-orange/index.html"
+  }
+];
